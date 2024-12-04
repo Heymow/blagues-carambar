fed22aa4a39a1ec239e3e4e7471643707d17b265
Khaled CHEHAB <lynckit@hotmail.com>
2024-12-04 11:44:49 +0100

    Mise à jour du chemin de l'image du logo dans le composant Header

--- src/composants/Header.js
+++ src/composants/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import styles from '../styles/Header.module.css';
 
 const Header = () => {
+    const imageSrc = `${process.env.PUBLIC_URL}/Logo.png`;
     return (
         <header className={styles.header}>
-            <img src="/logo.png" alt="Carambar & Co Logo" className={styles.logo} />
+            <img src={imageSrc} alt="Carambar & Co Logo" className={styles.logo} />
             <p className={styles.subtitle}>Des blagues 100% originales pour illuminer votre journée !</p>
         </header>
     );
